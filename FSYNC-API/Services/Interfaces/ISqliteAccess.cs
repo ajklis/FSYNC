5182168a7cdf22a93876da2eb934b3b109531a9f
Adam <adamjanklis@gmail.com>
2024-04-27 21:38:33 +0200

    Add project files.

---
+++ FSYNC-API/Services/Interfaces/ISqliteAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace FSYNC_API.Services.Interfaces
+{
+    internal interface ISqliteAccess
+    {
+        public Task<bool> AddRecord(string recordName, string path);
+        public Task<bool> RemoveRecord(string recordName);
+    }
+}
